5e2fd8ecbcd18efe8d1229fbf04f9261c76d10cf
Royal rana <royalrana2007@gmail.com>
2026-02-22 15:14:42 +0530

    Add files via upload

---
+++ dymamic_memory.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void add(int &a,int &b,int &sum)
+{
+    sum=a+b;
+}
+int main()
+{
+    int *a=new int;
+    int *b=new int;
+    int *sum=new int;
+    cout<<"enter any number :";
+    cin>>*a;
+    cout<<"enter any number :";
+    cin>>*b;
+    add(*a,*b,*sum);
+    cout<<"the sum is :"<<*sum;
+    return 0;
+
+}
