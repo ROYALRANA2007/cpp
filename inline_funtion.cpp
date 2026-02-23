de1aec0d2d53624297f6bd6f81028122ccbde4e5
Royal rana <royalrana2007@gmail.com>
2026-02-23 14:24:40 +0530

    Add files via upload

---
+++ inline_funtion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+    return a+b;
+    
+}
+int main()
+{
+    int a,b;
+     cout<<"enter any number :";
+    cin>>a;
+    cout<<"enter any number :";
+    cin>>b;
+    cout<<"sum is: "<<sum(a,b);
+    return 0;
+}
+ 
