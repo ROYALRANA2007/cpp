6952f03b63b870e1bd2cd4fb0a394758c41d7abe
Royal rana <royalrana2007@gmail.com>
2026-03-14 12:31:39 +0530

    Add files via upload

---
+++ Largest_Number_Using_Array_in_Class.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class{
+private :
+int n,max=0;
+int num[n];
+public:
+void input()
+{
+    #include <iostream>
+using namespace std;
+
+class Numbers
+{
+private:
+    int arr[100], n;
+
+public:
+    void input()
+    {
+        cout << "Enter size of array: ";
+        cin >> n;
+
+        cout << "Enter elements:\n";
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+    }
+
+    void display()
+    {
+        cout << "Array elements are:\n";
+        for(int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+};
+
+int main()
+{
+    Numbers n1;
+
+    n1.input();
+    n1.display();
+
+    return 0;
+}
+}
+
+};
